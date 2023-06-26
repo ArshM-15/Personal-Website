10c1898fdd5bfae9d6964d522beabda0a2381486
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-06-26 17:19:32 -0400

    fixed a hover related bug

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -15,7 +15,9 @@ const Navbar = () => {
         <Link to="contact" spy={true} smooth={true} offset={50} duration={700}>
           Contact
         </Link>
-        <a href="https://www.canva.com/design/DAFfGWvoXrM/view">Resume</a>
+        <a href="https://www.canva.com/design/DAFkK81NxfE/Wd3m9oKWJqdufqjopoY1sA/view?utm_content=DAFkK81NxfE&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink">
+          Resume
+        </a>
       </div>
     </div>
   );
